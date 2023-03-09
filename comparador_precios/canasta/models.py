90268a4f1498b8232e5adc626ec50b3c4f12e4d5
Francisco <victoriafrancis2009@gmail.com>
2023-03-09 18:08:46 -0300

    creacion y prueba de modelo de datos

--- comparador_precios/canasta/models.py
+++ comparador_precios/canasta/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    brand = models.CharField(max_length=30)
+    format = models.CharField(max_length=30)
+    measurementUnit = models.CharField(max_length=30)
+    
+    def __str__(self) -> str:
+        return f'{self.name} {self.brand} {self.format}'
+    
+class Supermarket(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Sells(models.Model):
+    unitPrice = models.IntegerField(null=False)
+    detailURL = models.URLField(default='')
+    item = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
+    supermarket = models.ForeignKey(Supermarket, on_delete=models.CASCADE, default=1)
+
+    def __str__(self) -> str:
+        return f"{self.item} {self.supermarket} {self.unitPrice}"
+
+
